a7f0ae8b0a359028ef2faaf894ea1cd59e7c583e
Julian Torreno <Skorezore@gmail.com>
2016-01-23 13:11:26 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# skorezore.github.io ![Mistake Badge](https://img.shields.io/badge/mistake-yes-red.svg)
+
+I think I can call myself a web developer now.
---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    particleground(document.getElementById('particles'),
+    {
+        dotColor: '#5cbdaa',
+        lineColor: 'transparent'
+    });
+}, false);
